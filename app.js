e80d5e6760fe955b2ce7c8683ee69fa0ec7397dc
Franklin <fchavezbaltodano.gmail.com>
2023-01-23 15:45:03 -0600

    estructura API

---
+++ app.js
@@ -0,0 +1,12 @@
+
+//Importamos 
+const Server = require('./models/server');
+//Creamos una nueva variable
+const server=new Server();
+//Invocamos el metodo listen
+server.listen();
+
+
+
+
+
---
+++ models/server.js
@@ -0,0 +1,48 @@
+const express= require('express');
+const cors=require('cors');
+require('dotenv').config();
+
+
+class Server{
+    constructor(){
+     this.app=express();
+     this.port=process.env.PORT;
+     this.usersPath='/api/users';
+     this.middlewares();
+     this.routes();
+    }
+
+
+
+    //Metodo que contiene las rutas
+routes(){
+//Creamos la primera peticion 
+this.app.use(this.usersPath,require('../routes/users'));
+
+}
+
+
+
+
+//Funciones que tiene el express y que me permite usarlas reutilizando codigo
+middlewares(){
+    this.app.use(express.static('public'));
+    this.app.use(cors());
+    //Habilitar el parseo de los datos del body
+    this.app.use(express.json());
+    
+}
+
+
+
+
+
+listen(){
+    this.app.listen(this.port, ()=>{ 
+         console.log(`El servidor esta corriendo en el puerto ${this.port}`);
+    });
+}
+
+
+}
+module.exports=Server;
